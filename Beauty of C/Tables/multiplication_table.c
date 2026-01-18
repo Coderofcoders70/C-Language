98a9c213948783627e0dcf6e06ac917484b67bb5
Coderofcoders70 <Lakshayas918@gmail.com>
2026-01-18 11:57:30 +0530

    Updated beauty of c

--- Beauty of C/Tables/multiplication_table.c
+++ Beauty of C/Tables/multiplication_table.c
@@ -1,5 +1,5 @@
 /*
-    Ques. To print multiplication table from 1 to n value
+    Ques. To print multiplication table from 1 to n values
     1   2   3   4   5   6   7   8   9  10
     2   4   6   8  10  12  14  16  18  20
     3   6   9  12  15  18  21  24  27  30
