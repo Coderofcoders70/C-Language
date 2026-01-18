bb668647132f1af0ff3cba4a9900e0e31ccb1bf5
Coderofcoders70 <Lakshayas918@gmail.com>
2026-01-18 12:03:08 +0530

    My first programm in c

---
+++ First Programm in C/first.c
@@ -0,0 +1,10 @@
+/* The term '\r' above is known as "Carriage return" which means it will return the cursor again to the beginning of the line. */
+
+#include <stdio.h>
+int main(void)
+{
+    printf("Hello world!\r");
+    printf("Good morning!\n");
+    printf("Heyy");
+    return 0;
+}
